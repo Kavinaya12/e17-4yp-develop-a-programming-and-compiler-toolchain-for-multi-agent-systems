65e519ddf10615caa60cfc86ebe8f6e20814b776
Kavinaya12 <e17159@eng.pdn.ac.lk>
2023-06-26 17:26:24 +0530

    mqtt connection issue fixed

--- code/client/src/pages/Mqtt/Connection.js
+++ code/client/src/pages/Mqtt/Connection.js
@@ -1,30 +1,34 @@
-import React from 'react';
-import { Card, Button, Form, Input, Row, Col } from 'antd';
+import React from "react";
+import { Card, Button, Form, Input, Row, Col } from "antd";
 
 const Connection = ({ connect, disconnect, connectBtn }) => {
   const [form] = Form.useForm();
   const record = {
-    host: 'broker.emqx.io',
-    clientId: `swarm_client + ${Math.random().toString(16).substr(2, 8)}`,
-    port: 8083,
+    host: "68.183.188.135",
+    // "broker.emqx.io",
+    clientId: `Robot_ + ${Math.random().toString(16).substr(2, 8)}`,
+    // swarm_client
+    port: 9001,
+    username: "swarm_user",
+    password: "swarm_usere15",
   };
   const onFinish = (values) => {
     const { host, clientId, port, username, password } = values;
-    const url = `ws://${host}:${port}/mqtt`;
+    const url = `ws://${host}:${port}/ws`;
     const options = {
       keepalive: 30,
-      protocolId: 'MQTT',
+      protocolId: "MQTT",
       protocolVersion: 4,
       clean: true,
       reconnectPeriod: 1000,
       connectTimeout: 30 * 1000,
       will: {
-        topic: 'WillMsg',
-        payload: 'Connection Closed abnormally..!',
+        topic: "WillMsg",
+        payload: "Connection Closed abnormally..!",
         qos: 0,
-        retain: false
+        retain: false,
       },
-      rejectUnauthorized: false
+      rejectUnauthorized: false,
     };
     options.clientId = clientId;
     options.username = username;
@@ -50,60 +54,49 @@ const Connection = ({ connect, disconnect, connectBtn }) => {
     >
       <Row gutter={20}>
         <Col span={8}>
-          <Form.Item
-            label="Host"
-            name="host"
-          >
+          <Form.Item label="Host" name="host">
             <Input />
           </Form.Item>
         </Col>
         <Col span={8}>
-          <Form.Item
-            label="Port"
-            name="port"
-          >
+          <Form.Item label="Port" name="port">
             <Input />
           </Form.Item>
         </Col>
         <Col span={8}>
-          <Form.Item
-            label="Client ID"
-            name="clientId"
-          >
+          <Form.Item label="Client ID" name="clientId">
             <Input />
           </Form.Item>
         </Col>
         <Col span={8}>
-          <Form.Item
-            label="Username"
-            name="username"
-          >
+          <Form.Item label="Username" name="username">
             <Input />
           </Form.Item>
         </Col>
         <Col span={8}>
-          <Form.Item
-            label="Password"
-            name="password"
-          >
+          <Form.Item label="Password" name="password">
             <Input />
           </Form.Item>
         </Col>
       </Row>
     </Form>
-  )
+  );
 
   return (
     <Card
       title="Connection"
       actions={[
-        <Button type="primary" onClick={handleConnect}>{connectBtn}</Button>,
-        <Button danger onClick={handleDisconnect}>Disconnect</Button>
+        <Button type="primary" onClick={handleConnect}>
+          {connectBtn}
+        </Button>,
+        <Button danger onClick={handleDisconnect}>
+          Disconnect
+        </Button>,
       ]}
     >
       {ConnectionForm}
     </Card>
   );
-}
+};
 
 export default Connection;
